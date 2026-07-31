83ebd08bcf1391fa455887a0ab3a9818fe7ce76c
Tejas <tejass4825@gmail.com>
2026-07-31 21:54:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.86%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long rev = 0;
+        int ld = 0;
+
+        while(x != 0){
+            ld = x%10;
+            x = x/10;
+            rev = rev*10 + ld;
+        }
+
+    if(rev > INT_MAX || rev < INT_MIN){
+        return 0;
+    }
+
+        return rev;
+    }
+};
