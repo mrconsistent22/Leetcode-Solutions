27b00c53a649d4fd87a27e43e390f22cb792527a
Tejas <tejass4825@gmail.com>
2026-09-14 23:20:01 +0530

    Time: 0 ms (100%), Space: 14.9 MB (44.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -12,15 +12,36 @@ public:
         // }
         // return {};
 
-    map<int, int> mpp;
-    for(int i = 0; i<nums.size(); i++){
-        int more = target - nums[i];
-        if(mpp.find(more) != mpp.end()){
-            return {mpp[more], i};
+    // map<int, int> mpp;
+    // for(int i = 0; i<nums.size(); i++){
+    //     int more = target - nums[i];
+    //     if(mpp.find(more) != mpp.end()){
+    //         return {mpp[more], i};
+    //     }
+    //     mpp[nums[i]] = i;
+    // }
+    // return {};
+
+
+    vector<pair<int, int>> arr;
+
+    for(int i = 0; i < nums.size(); i++) {
+        arr.push_back({nums[i], i});
+    }
+    sort(arr.begin(), arr.end());
+    int i = 0;
+    int j = nums.size() - 1;
+
+    while(i < j){
+        int sum = arr[i].first + arr[j].first;
+        if(sum == target){
+            return {arr[i].second, arr[j].second};
         }
-        mpp[nums[i]] = i;
+        else if(sum < target) i++;
+        else j--;
     }
     return {};
+
 
     }
 };
