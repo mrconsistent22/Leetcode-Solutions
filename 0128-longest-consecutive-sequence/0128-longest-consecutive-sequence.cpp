fd2f86854dda334db6c8006a022b4d05de451ae9
Tejas <tejass4825@gmail.com>
2026-09-21 10:04:03 +0530

    Time: 76 ms (70.69%), Space: 88.9 MB (48.97%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        sort(nums.begin(), nums.end());
-        int longest = 1;
-        int lastSmall = INT_MIN;
-        int count = 0;
+        // if(nums.empty()) return 0;
+        // sort(nums.begin(), nums.end());
+        // int longest = 1;
+        // int lastSmall = INT_MIN;
+        // int count = 0;
         
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]-1 == lastSmall){
-                count++;
-                lastSmall = nums[i];
-            }else if(nums[i] == lastSmall){
-                continue;
-            }else{
-                count = 1;
-                lastSmall = nums[i];
+        // for(int i = 0; i<nums.size(); i++){
+        //     if(nums[i]-1 == lastSmall){
+        //         count++;
+        //         lastSmall = nums[i];
+        //     }else if(nums[i] == lastSmall){
+        //         continue;
+        //     }else{
+        //         count = 1;
+        //         lastSmall = nums[i];
+        //     }
+        //     longest = max(longest, count);
+        // }
+        // return longest;
+
+        if(nums.empty()) return 0;
+        int longest = 1;
+        unordered_set<int> st;
+        for(int  i = 0; i<nums.size(); i++){
+            st.insert(nums[i]);
+        }
+
+        for(auto it : st){
+            if(st.find(it - 1) == st.end()){
+                int count = 1;
+                int x=it;
+
+                while(st.find(x+1) != st.end()){
+                    x++;
+                    count++;
+                }
+                longest = max(longest, count);
             }
-            longest = max(longest, count);
         }
         return longest;
     }
