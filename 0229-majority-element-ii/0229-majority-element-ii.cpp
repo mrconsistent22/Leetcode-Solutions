110ce7160786a70a76c0063311e877b4f1838f5f
Tejas <tejass4825@gmail.com>
2026-09-25 22:43:07 +0530

    Time: 0 ms (100%), Space: 23.8 MB (76.74%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -22,17 +22,56 @@ public:
         // }
         // return ans;
 
-        map<int, int> mpp;
-        int threshold = (nums.size())/3 + 1;
-        for(int i = 0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]] == threshold){
-                ans.push_back(nums[i]);
-            }
-            if(ans.size() == 2){
-                break;
-            }
+        // map<int, int> mpp;
+        // int threshold = (nums.size())/3 + 1;
+        // for(int i = 0; i<nums.size(); i++){
+        //     mpp[nums[i]]++;
+        //     if(mpp[nums[i]] == threshold){
+        //         ans.push_back(nums[i]);
+        //     }
+        //     if(ans.size() == 2){
+        //         break;
+        //     }
+        // }
+        // return ans;
+
+
+    int count1 = 0, count2 = 0;
+    int ele1 = 0, ele2 = 0;
+
+
+    for(int x : nums) {
+        if(x == ele1)
+            count1++;
+        else if(x == ele2)
+            count2++;
+        else if(count1 == 0) {
+            ele1 = x;
+            count1 = 1;
         }
-        return ans;
+        else if(count2 == 0) {
+            ele2 = x;
+            count2 = 1;
+        }
+        else {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = count2 = 0;
+
+    for(int x : nums) {
+        if(x == ele1) count1++;
+        else if(x == ele2) count2++;
+    }
+
+    if(count1 > nums.size() / 3)
+        ans.push_back(ele1);
+
+    if(count2 > nums.size() / 3)
+        ans.push_back(ele2);
+
+    return ans;
     }
 };
